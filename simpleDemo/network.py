b816d1f4bee174678701f9c06693de7ae438327b
soong <shikaisoong@gmail.com>
2020-05-16 14:01:06 +0800

    simple demo fo neural network to implement SGD

--- simpleDemo/network.py
+++ simpleDemo/network.py
@@ -32,12 +32,12 @@ class Network(object):
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)]
-        for mini_batch in mini_batches:
-            self.update_mini_batch(mini_batch, eta)
-        if test_data:
-            print ("Epoch {0}: {1}/{2}".format(j, self.evaluate(test_data), n_test))
-        else:
-            print ("Epoch {0} complete".format(j))
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print ("Epoch {0}: {1}/{2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print ("Epoch {0} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying gradient descent using backpropagation"""
@@ -46,7 +46,7 @@ class Network(object):
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
--- simpleDemo/test.py
+++ simpleDemo/test.py
@@ -2,5 +2,5 @@ import mnist_loader
 import network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
+net = network.Network([784, 50, 10])
+net.SGD(training_data, 30, 10, 2.9, test_data = test_data)
